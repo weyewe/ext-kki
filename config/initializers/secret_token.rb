52be3d7a0f7230cf30a4e265f4417d6e7f31a5bd
weyewe <w.yunnal@gmail.com>
2013-04-14 14:11:40 +0700

    simple design

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Neokki::Application.config.secret_token = 'f1bbdf72dffc317d74ba45d5d66a9e1c40c70866e88c1482fa1fa509daf6b77110b03e50d672c7a57fbd06f46bd59be5fd810c80c7f6fd22586a58254a0f1747'
